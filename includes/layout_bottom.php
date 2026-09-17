ce974b27e4dd2a1db3cf7cbb6cf159d05bd6dd78
webautista <emmanuel_bautista@hotmail.com>
2026-09-17 15:19:25 -0400

    colapsar

--- includes/layout_bottom.php
+++ includes/layout_bottom.php
@@ -1,6 +1,6 @@
     </div>
   </div>
 </div>
-<script src="<?= e($base) ?>/assets/js/app.js"></script>
+<script src="<?= e($base) ?>/assets/js/app.js?v=<?= @filemtime(__DIR__ . '/../assets/js/app.js') ?: '1' ?>"></script>
 </body>
 </html>
